59ce6fd36bb40c06dbcd5a796418fd8e2697ac43
Kalyani02072003 <71889838+Kalyani02072003@users.noreply.github.com>
2022-04-13 10:36:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import Main from "./components/Main"
+import Content from "./components/Content"
+import Footer from "./components/Footer"
+import { library } from '@fortawesome/fontawesome-svg-core'
+export default function App() {
+    return (
+        <div>
+            <Main />
+            <Content />
+            <Footer />
+        </div>
+    )
 }
-
-export default App;
